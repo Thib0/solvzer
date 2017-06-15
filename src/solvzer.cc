4e20ea4dec7f28061524e9fbdbd409f666cc2b13
Thibault Liotier <liotie_t@epita.fr>
2017-06-16 01:32:44 +0200

    display: 2D display for each move

--- src/solvzer.cc
+++ src/solvzer.cc
@@ -6,34 +6,12 @@
 
 int main(int argc, char** argv)
 {
-  SDL_Window *window = SDL_CreateWindow("SolvZer", SDL_WINDOWPOS_CENTERED,
-      SDL_WINDOWPOS_CENTERED,
-      1280, 680, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-  SDL_Renderer *renderer =  SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  auto display = display::Display(window, renderer);
-  //display.setup_background();
-
+  SDL_Window *window = nullptr;
+  SDL_Renderer *renderer = nullptr;
   SDL_Event event;
-  auto rub = "UUUUUUUUU";
-  display.draw_rubiks(rub);
-  /*while (true)
-  {
-    SDL_WaitEvent(&event);
-    if(event.window.event == SDL_WINDOWEVENT_CLOSE)
-      break;*/
-  //}
-  SDL_Delay(10000);
-  return 0;
+  auto display = display::Display::Instance(&window, &renderer);
+
   cube::Search search;
-
-  /*auto face = cube::Face(init_state);
-
-  std::cout << face << std::endl;
-
-  auto solution = search.solution(init_state, DEPTH);
-
-  if (!search.ack_solution(init_state, solution))
-    std::cout << "Solution is wrong" << std::endl; */
 
 
   auto face = cube::Face(cube::Cube::solved_state_);
