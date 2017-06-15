d17a9e159a1ef0ed2fd723cc0bf910e7e73c497f
Thibault Liotier <liotie_t@epita.fr>
2017-06-15 20:36:56 +0200

    display: add basic draw for one face

--- src/solvzer.cc
+++ src/solvzer.cc
@@ -1,11 +1,29 @@
 #include <cassert>
 #include <iostream>
-
 #include <cube/search.hh>
 #include <detect/detector.hh>
+#include <misc/display.hh>
 
 int main(int argc, char** argv)
 {
+  SDL_Window *window = SDL_CreateWindow("SolvZer", SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      1280, 680, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+  SDL_Renderer *renderer =  SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  auto display = display::Display(window, renderer);
+  //display.setup_background();
+
+  SDL_Event event;
+  auto rub = "UUUUUUUUU";
+  display.draw_rubiks(rub);
+  /*while (true)
+  {
+    SDL_WaitEvent(&event);
+    if(event.window.event == SDL_WINDOWEVENT_CLOSE)
+      break;*/
+  //}
+  SDL_Delay(10000);
+  return 0;
   cube::Search search;
 
   /*auto face = cube::Face(init_state);
