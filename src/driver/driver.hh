261c5aa4dcf94c302f5f9b01506643d276ed384a
Guillaume Marques <guillaume.marques@epita.fr>
2017-08-07 18:54:02 +0200

    driver: add driver class
    * driver.hh: Here.

---
+++ src/driver/driver.hh
@@ -0,0 +1,23 @@
+/**
+ ** \file driver/driver.hh
+ ** \author Guillaume Marques
+ ** \version 1.0
+ ** \brief Link driver between hardware and software.
+ **/
+
+#pragma once
+
+namespace driver
+{
+
+  /** \class Driver
+   ** \brief Driver class.
+   **/
+  class Driver
+  {
+  public:
+
+  private:
+  };
+
+} // namespace driver
